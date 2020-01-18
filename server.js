1c9ff84590481cdc32b46bf69c3ea8a10937253d
Elisabeth Hansen <bethhansen92@gmail.com>
2020-01-18 15:17:57 -0800

    Finished feature 2

--- server.js
+++ server.js
@@ -10,8 +10,32 @@ const app = express();
 app.use(cors());
 
 app.get('/', (request, response) => {
-  response.send('Home Page!');
+  response.send('This still works!');
 });
+
+app.get('/location', (request, response) => {
+  try {
+    const geoData = require('./data/geo.json');
+    const city = request.query.city;
+    const locationData = new Location (city, geoData);
+    response.send(locationData);
+  }
+  catch(error) {
+    errorHandler('So sorry, something went wrong.', request, response);
+  }
+});
+
+function errorHandler(error, request, response) {
+  response.status(500).send(error);
+}
+
+function Location(city, geoData){
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
 
 
 
